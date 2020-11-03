dc0f9db0d6d7579751b0807185f166118fe33d87
PeppoNetwork <58642401+PeppoNetwork@users.noreply.github.com>
2020-11-03 21:48:37 +0200

    Update OffhandModule.java

--- src/main/java/me/ionar/salhack/module/combat/OffhandModule.java
+++ src/main/java/me/ionar/salhack/module/combat/OffhandModule.java
@@ -23,95 +23,147 @@ public final class OffhandModule extends Module
     
     public enum Modes
     {
-        Gap,
-        Crystal,
-        Bow,
+        
+        Crystal.
+        
     }
     
-    public OffhandModule()
-    {
-        super("Offhand", new String[]
-        { "Totem" }, "Pauses AutoTotem and places something else in your offhand.", "NONE", 0xDADB24, ModuleType.COMBAT);
-    }
+    public OffhandModule(){
+		super("OffhandCrystal", Category.Combat);
+		this.moving = false;
+		this.returnI = false;
+	}
 
-    @Override
-    public String getMetaData()
-    {
-        return String.valueOf(Mode.getValue());
-    }
-    
-    private void SwitchOffHandIfNeed(Modes p_Val)
-    {
-        Item l_Item = GetItemFromModeVal(p_Val);
-        
-        if (mc.player.getHeldItemOffhand().getItem() != l_Item)
-        {
-            int l_Slot = HotbarFirst.getValue() ? PlayerUtil.GetRecursiveItemSlot(l_Item) : PlayerUtil.GetItemSlot(l_Item);
-            
-            if (l_Slot != -1)
-            {
-                mc.playerController.windowClick(mc.player.inventoryContainer.windowId, l_Slot, 0,
-                        ClickType.PICKUP, mc.player);
-                mc.playerController.windowClick(mc.player.inventoryContainer.windowId, 45, 0, ClickType.PICKUP,
-                        mc.player);
-                
-                /// @todo: this might cause desyncs, we need a callback for windowclicks for transaction complete packet.
-                mc.playerController.windowClick(mc.player.inventoryContainer.windowId, l_Slot, 0,
-                        ClickType.PICKUP, mc.player);
-                mc.playerController.updateController();
-                
-                SendMessage(ChatFormatting.LIGHT_PURPLE + "Offhand now has a " + GetItemNameFromModeVal(Mode.getValue()));
-            }
-        }
-    }
+	@Override
+	public void setup(){
+		health = registerInteger("Health", "Health", 15, 0, 36);
+	}
 
-    @EventHandler
-    private Listener<EventPlayerUpdate> OnPlayerUpdate = new Listener<>(p_Event ->
-    {
-        if (mc.currentScreen != null && (!(mc.currentScreen instanceof GuiInventory) && !(mc.currentScreen instanceof SalGuiScreen)))
-            return;
-        
-        if (PlayerUtil.GetHealthWithAbsorption() < ToggleHealth.getValue())
-        {
-            SendMessage("You are below the ToggleHealth requirement, toggling..");
-            toggle();
-            return;
-        }
-        
-        SwitchOffHandIfNeed(Mode.getValue());
-    });
-    
-    public Item GetItemFromModeVal(Modes p_Val)
-    {
-        switch (p_Val)
-        {
-            case Crystal:
-                return Items.END_CRYSTAL;
-            case Gap:
-                return Items.GOLDEN_APPLE;
-            case Bow:
-                return Items.BOW;
-            default:
-                break;
-        }
-        
-        return Items.TOTEM_OF_UNDYING;
-    }
+	public void onDisable(){
+		if (OffhandCrystal.mc.currentScreen instanceof GuiContainer){
+			return;
+		}
+		this.crystals = OffhandCrystal.mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING).mapToInt(ItemStack::getCount).sum();
+		if (OffhandCrystal.mc.player.getHeldItemOffhand().getItem() != Items.TOTEM_OF_UNDYING){
+			if (this.crystals == 0){
+				return;
+			}
+			int t = -1;
+			for (int i = 0; i < 45; i++){
+				if (OffhandCrystal.mc.player.inventory.getStackInSlot(i).getItem() == Items.TOTEM_OF_UNDYING){
+					t = i;
+					break;
+				}
+			}
+			if (t == -1){
+				return;
+			}
+			OffhandCrystal.mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+			OffhandCrystal.mc.playerController.windowClick(0, (t < 9) ? (t + 36) : t, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+			OffhandCrystal.mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+		}
+	}
 
-    private String GetItemNameFromModeVal(Modes p_Val)
-    {
-        switch (p_Val)
-        {
-            case Crystal:
-                return "End Crystal";
-            case Gap:
-                return "Gap";
-            case Bow:
-                return "Bow";
-            default:
-                break;
-        }
-        
-        return "Totem";
-    }
-}
+	@Override
+	public void onUpdate(){
+		this.item = Items.END_CRYSTAL;
+		if (OffhandCrystal.mc.currentScreen instanceof GuiContainer){
+			return;
+		}
+		if (this.returnI){
+			int t = -1;
+			for (int i = 0; i < 45; i++){
+				if (OffhandCrystal.mc.player.inventory.getStackInSlot(i).isEmpty()){
+					t = i;
+					break;
+				}
+			}
+			if (t == -1){
+				return;
+			}
+			OffhandCrystal.mc.playerController.windowClick(0, (t < 9) ? (t + 36) : t, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+			this.returnI = false;
+		}
+		this.totems = OffhandCrystal.mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == Items.TOTEM_OF_UNDYING).mapToInt(ItemStack::getCount).sum();
+		this.crystals = OffhandCrystal.mc.player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() == this.item).mapToInt(ItemStack::getCount).sum();
+		if (this.shouldTotem() && OffhandCrystal.mc.player.getHeldItemOffhand().getItem() == Items.TOTEM_OF_UNDYING){
+			this.totems++;
+		} else if (!this.shouldTotem() && OffhandCrystal.mc.player.getHeldItemOffhand().getItem() == this.item){
+			this.crystals += OffhandCrystal.mc.player.getHeldItemOffhand().getCount();
+		} else{
+			if (this.moving){
+				OffhandCrystal.mc.playerController.windowClick(0, 45, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+				this.moving = false;
+				this.returnI = true;
+				return;
+			}
+			if (OffhandCrystal.mc.player.inventory.getItemStack().isEmpty()){
+				if (!this.shouldTotem() && OffhandCrystal.mc.player.getHeldItemOffhand().getItem() == this.item){
+					return;
+				}
+				if (this.shouldTotem() && OffhandCrystal.mc.player.getHeldItemOffhand().getItem() == Items.TOTEM_OF_UNDYING){
+					return;
+				}
+				if (!this.shouldTotem()){
+					if (this.crystals == 0){
+						return;
+					}
+					int t = -1;
+					for (int i = 0; i < 45; i++){
+						if (OffhandCrystal.mc.player.inventory.getStackInSlot(i).getItem() == this.item){
+							t = i;
+							break;
+						}
+					}
+					if (t == -1){
+						return;
+					}
+					OffhandCrystal.mc.playerController.windowClick(0, (t < 9) ? (t + 36) : t, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+					this.moving = true;
+				} else{
+					if (this.totems == 0){
+						return;
+					}
+					int t = -1;
+					for (int i = 0; i < 45; i++){
+						if (OffhandCrystal.mc.player.inventory.getStackInSlot(i).getItem() == Items.TOTEM_OF_UNDYING){
+							t = i;
+							break;
+						}
+					}
+					if (t == -1){
+						return;
+					}
+					OffhandCrystal.mc.playerController.windowClick(0, (t < 9) ? (t + 36) : t, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+					this.moving = true;
+				}
+			} else{
+				int t = -1;
+				for (int i = 0; i < 45; i++){
+					if (OffhandCrystal.mc.player.inventory.getStackInSlot(i).isEmpty()){
+						t = i;
+						break;
+					}
+				}
+				if (t == -1){
+					return;
+				}
+				OffhandCrystal.mc.playerController.windowClick(0, (t < 9) ? (t + 36) : t, 0, ClickType.PICKUP, OffhandCrystal.mc.player);
+			}
+		}
+	}
+
+	private boolean shouldTotem(){
+		final boolean hp = OffhandCrystal.mc.player.getHealth() + OffhandCrystal.mc.player.getAbsorptionAmount() <= health.getValue();
+		final boolean endcrystal = !this.isCrystalsAABBEmpty();
+		return hp;
+	}
+
+	private boolean isEmpty(final BlockPos pos){
+		final List<Entity> crystalsInAABB = OffhandCrystal.mc.world.getEntitiesWithinAABBExcludingEntity(null, new AxisAlignedBB(pos)).stream().filter(e -> e instanceof EntityEnderCrystal).collect(Collectors.toList());
+		return crystalsInAABB.isEmpty();
+	}
+
+	private boolean isCrystalsAABBEmpty(){
+		return this.isEmpty(OffhandCrystal.mc.player.getPosition().add(1, 0, 0)) && this.isEmpty(OffhandCrystal.mc.player.getPosition().add(-1, 0, 0)) && this.isEmpty(OffhandCrystal.mc.player.getPosition().add(0, 0, 1)) && this.isEmpty(OffhandCrystal.mc.player.getPosition().add(0, 0, -1)) && this.isEmpty(OffhandCrystal.mc.player.getPosition());
+	}
